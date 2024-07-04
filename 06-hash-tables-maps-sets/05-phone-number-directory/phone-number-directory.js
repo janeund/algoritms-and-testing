74b359332d3f0ccc6bfb94516eaca4f55a59f692
Evgeniia Zhavoronkova <maongaku@gmail.com>
2024-07-04 15:42:18 +0200

    feat: add phone-number-directory using Map challenge solution

--- 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
+++ 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
@@ -1,3 +1,10 @@
-function phoneNumberDirectory() {}
+function phoneNumberDirectory(arr) {
+  const directory = new Map();
+  for (const entry of arr) {
+   const [name, phoneNumber] = entry.split(':');
+   directory.set(name, phoneNumber)
+  }
+  return phoneMap
+}
 
 module.exports = phoneNumberDirectory;
