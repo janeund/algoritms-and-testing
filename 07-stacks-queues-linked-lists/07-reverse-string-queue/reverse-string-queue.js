43534b800745629632f9ab96eb3b0d601a964cab
Evgeniia Zhavoronkova <maongaku@gmail.com>
2024-07-05 14:53:29 +0200

    feat: add reversed-string- queue solution

--- 07-stacks-queues-linked-lists/07-reverse-string-queue/reverse-string-queue.js
+++ 07-stacks-queues-linked-lists/07-reverse-string-queue/reverse-string-queue.js
@@ -1,5 +1,17 @@
 const Queue = require('./queue');
 
-const reverseStringWithQueue = (str) => {};
+const reverseStringWithQueue = (str) => {
+  const queue = new Queue();
+  for (let i = str.length - 1; i >= 0; i--) {
+    queue.enqueue(str[i])
+  }
+  let reversedStr = '';
+  while (!queue.isEmpty()) {
+    reversedStr += queue.dequeue()
+  }
+  return reversedStr
+};
+
+console.log(reverseStringWithQueue('hello'));
 
 module.exports = reverseStringWithQueue;
