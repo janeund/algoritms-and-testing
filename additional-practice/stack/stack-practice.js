0bc8cdac6d99f0808c82648427d0ae07f06a01b1
Evgeniia Zhavoronkova <maongaku@gmail.com>
2024-07-06 13:25:27 +0200

    feat: add another practice files

---
+++ additional-practice/stack/stack-practice.js
@@ -0,0 +1,5 @@
+const Stack = require('./stack');
+
+// 1. Write a JavaScript program to implement a stack with push and pop operations. 
+// Find the top element of the stack and check if it is empty or not.
+
