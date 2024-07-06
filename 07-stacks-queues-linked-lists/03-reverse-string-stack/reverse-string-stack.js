60dd91a7e39f80b0f385757efb656afca4410f92
Evgeniia Zhavoronkova <maongaku@gmail.com>
2024-07-06 13:02:52 +0200

    feat: add stack-reversed-string solution

--- 07-stacks-queues-linked-lists/03-reverse-string-stack/reverse-string-stack.js
+++ 07-stacks-queues-linked-lists/03-reverse-string-stack/reverse-string-stack.js
@@ -1,5 +1,16 @@
 const Stack = require('./stack');
 
-function reverseStringStack() {}
+function reverseStringStack(str) {
+  const stack = new Stack();
+  
+  for (let i = 0; i < str.length; i++) {
+    stack.push(str[i])
+  }
+  const reversedString = '';
+  while (!stack.isEmpty) {
+    reversedString += stack.pop()
+  }
+  return reversedString;
+}
 
 module.exports = reverseStringStack;
