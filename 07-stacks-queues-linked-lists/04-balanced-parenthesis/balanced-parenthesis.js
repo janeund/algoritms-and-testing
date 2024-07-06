419e68a9f788969a174cfb974697e552860a62c9
Evgeniia Zhavoronkova <maongaku@gmail.com>
2024-07-06 13:41:27 +0200

    feat: add stack-balanced-parenthesis solution

--- 07-stacks-queues-linked-lists/04-balanced-parenthesis/balanced-parenthesis.js
+++ 07-stacks-queues-linked-lists/04-balanced-parenthesis/balanced-parenthesis.js
@@ -4,15 +4,15 @@ function balancedParenthesis(str) {
   const stack = new Stack();
   for (let i = 0; i < str.length; i++) {
     if (str[i] === '(') {
-      stack.push(str[i]);
+      stack.push(str[i])
     } else if (str[i] === ')') {
       if (stack.isEmpty()) {
-        return false
-      } else {
-        stack.pop()
+        return false;
       }
+      stack.pop();
     }
   }
+  return stack.isEmpty()
 }
 
 module.exports = balancedParenthesis;
