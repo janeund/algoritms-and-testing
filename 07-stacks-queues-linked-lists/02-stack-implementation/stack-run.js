4618411a5763dfe00a7ed249613a168124b46f88
Evgeniia Zhavoronkova <maongaku@gmail.com>
2024-07-06 12:52:02 +0200

    feat: add stack-implementation solution

--- 07-stacks-queues-linked-lists/02-stack-implementation/stack-run.js
+++ 07-stacks-queues-linked-lists/02-stack-implementation/stack-run.js
@@ -1,2 +1,11 @@
 const Stack = require('./stack');
 
+const stack = new Stack();
+
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.push(4);
+
+console.log(stack);
+
--- 07-stacks-queues-linked-lists/02-stack-implementation/stack.js
+++ 07-stacks-queues-linked-lists/02-stack-implementation/stack.js
@@ -1,3 +1,36 @@
-class Stack {}
+class Stack {
+  constructor () {
+    this.stack = [];
+    this.maxSize = 3;
+    this.top = -1;
+  }
+  push(value) {
+    if (this.isFull()) {
+      return false;
+    }
+    this.top++;
+    this.stack[this.top] = value;
+    return true;
+  }
+  pop() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    this.top--;
+    return this.stack.pop();
+  }
+  peek() {
+    if (this.isEmpty) {
+      return null;
+    }
+    return this.stack[this.top];
+  }
+  isFull() {
+    return this.top === this.maxSize - 1;
+  }
+  isEmpty() {
+    return this.top === -1;
+  }
+}
 
 module.exports = Stack;
